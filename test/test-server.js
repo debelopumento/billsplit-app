64820720ca1c1ea4d8332c2ce40649259d85ec24
debelopumento <imdiye@gmail.com>
2017-02-27 17:36:24 -0800

    added colors module

--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,16 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../server.js');
+var chai = require('chai')
+var chaiHttp = require('chai-http')
+var server = require('../server.js')
 
-var should = chai.should();
-var app = server.app;
-var storage = server.storage;
 
-chai.use(chaiHttp);
+var should = chai.should()
+var app = server.app
+var storage = server.storage
+
+const faker = require('faker')
+const mongoose = require('mongoose')
+
+chai.use(chaiHttp)
 
 
 describe('index page', function() {
@@ -14,10 +18,51 @@ describe('index page', function() {
     chai.request(app)
       .get('/')
       .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
+        res.should.have.status(200)
+        res.should.be.html
+        done()
+    })
+  })
 
+/*
+  
+  it('should list all/up-to-100 bills in database', function() {
+	let res
+	return chai.request(app)
+		.get('/bills')
+		.then(function(_res) {
+			res = _res
+			res.should.have.status(200)
+			res.body.bills.should.have.length.of.at.least(1)
+			Bills
+			.count()
+			.then(function(count) {
+				res.body.bills.should.have.length.of(count)
+				done()
+			})
+   		})
+  })
+
+  it('should delete a bill', function() {
+  	let bill
+  	Bills
+  	.findOne()
+  	.exec()
+  	.then(function(_bill) {
+  		bill = _bill
+  		return chai.request(app).delete(`/bills/${bill.id)}`)
+  	})
+  	.then(function(res) {
+  		res.should.have.status(204)
+  		return Bills.findById(bill.id).exec()
+  	})
+  	.then(function(_bill) {
+  		should.not.exist(_bill)
+  	})
+  })
+
+*/
+
+})
+
+	
