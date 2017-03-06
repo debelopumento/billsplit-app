be70cfe5a8a59a43443be7bb1af0723fa6dbb988
debelopumento <imdiye@gmail.com>
2017-03-05 22:57:31 -0800

    added Facebook login and log out button

--- public/app.js
+++ public/app.js
@@ -477,9 +477,53 @@ function login(signedInUser) {
 
 
 $(function() {
-        
+        $(function() {
+            // This is called with the results from from FB.getLoginStatus().
+  
+              window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '1859697977624978',
+                cookie     : true,  // enable cookies to allow the server to access 
+                                    // the session
+                xfbml      : true,  // parse social plugins on this page
+                version    : 'v2.8' // use graph api version 2.8
+              });
+
+              // Now that we've initialized the JavaScript SDK, we call 
+              // FB.getLoginStatus().  This function gets the state of the
+              // person visiting this page and can return one of three states to
+              // the callback you provide.  They can be:
+              //
+              // 1. Logged into your app ('connected')
+              // 2. Logged into Facebook, but not your app ('not_authorized')
+              // 3. Not logged into Facebook and can't tell if they are logged into
+              //    your app or not.
+              //
+              // These three cases are handled in the callback function.
+
+              FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+                if(response.status === 'connected') {
+                  alert('hello')
+                }
+              });
+
+              };
+
+              // Load the SDK asynchronously
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+
+  
+        })
         $('header').toggleClass("hidden");
         var row = '';
+        row += '<fb:login-button autologoutlink="true"></fb:login-button>'
         row += '<p>Hello!</p><br>';
         row += '<input class="username" type="text" placeholder="username"><br>';
         row += '<input class="password" type="password" placeholder="password">';
