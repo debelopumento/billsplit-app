51edab32ec3a80f7a0a9324b4b46272a2f288d4c
debelopumento <imdiye@gmail.com>
2017-03-06 14:23:41 -0800

    turned off facebook default status

--- public/app.js
+++ public/app.js
@@ -12,19 +12,19 @@ function register(signedInUserFacebookId, signedInUserFullName) {
         newUser.fullName = $('.js-fullName').val();
         newUser.friends = [];
         $.ajax({
-                    type: "POST",
-                    url: window.location.href + 'users/',
-                    data: JSON.stringify(newUser),
-                    dataType: "json",
-                    contentType: "application/json",
-                    success: function(data) {
-                        var signedInUser = {user: data};
-                        login(signedInUser);
-                    },
-                    error: function(e) {
-                        console.log(e);
-                    }
-                });
+            type: "POST",
+            url: window.location.href + 'users/',
+            data: JSON.stringify(newUser),
+            dataType: "json",
+            contentType: "application/json",
+            success: function(data) {
+                var signedInUser = {user: data};
+                login(signedInUser);
+            },
+            error: function(e) {
+                console.log(e);
+            }
+        });
     });
 }
 
@@ -36,24 +36,24 @@ function login(signedInUser) {
 
     function addNewBill() {
         var newBill = {
-                totalAmount: 0,
-                billDate: Date(),
-                description: '',
-                users: [
-                    {userId: userId,
-                     fullName: userFullName,
-                     splitAmount: 0
-                    },
-                    {userId: '',
-                     fullName: '',
-                     splitAmount: 0
-                    }
-                ],
-                //dueDay: Date(),
-                postedTime: Date(),
-                paidByUser: {userId: userId, fullName: userFullName},
-                padiOff: false,
-                memo: ""
+            totalAmount: 0,
+            billDate: Date(),
+            description: '',
+            users: [
+                {userId: userId,
+                 fullName: userFullName,
+                 splitAmount: 0
+                },
+                {userId: '',
+                 fullName: '',
+                 splitAmount: 0
+                }
+            ],
+            //dueDay: Date(),
+            postedTime: Date(),
+            paidByUser: {userId: userId, fullName: userFullName},
+            padiOff: false,
+            memo: ""
         };
         var isNewBill = true;
         console.log(34, newBill)
@@ -477,80 +477,75 @@ $(function() {
         $(function() {
             // This is called with the results from from FB.getLoginStatus().
                 
-              window.fbAsyncInit = function() {
-                  FB.init({
-                    appId      : '1859697977624978',
-                    cookie     : true,  // enable cookies to allow the server to access 
-                                        // the session
-                    xfbml      : true,  // parse social plugins on this page
-                    version    : 'v2.8' // use graph api version 2.8
-                  });
+          window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '1859697977624978',
+                cookie     : true,  // enable cookies to allow the server to access 
+                                    // the session
+                xfbml      : true,  // parse social plugins on this page
+                version    : 'v2.8' // use graph api version 2.8
+              });
 
-                  // Now that we've initialized the JavaScript SDK, we call 
-                  // FB.getLoginStatus().  This function gets the state of the
-                  // person visiting this page and can return one of three states to
-                  // the callback you provide.  They can be:
-                  //
-                  // 1. Logged into your app ('connected')
-                  // 2. Logged into Facebook, but not your app ('not_authorized')
-                  // 3. Not logged into Facebook and can't tell if they are logged into
-                  //    your app or not.
-                  //
-                  // These three cases are handled in the callback function.
+              // Now that we've initialized the JavaScript SDK, we call 
+              // FB.getLoginStatus().  This function gets the state of the
+              // person visiting this page and can return one of three states to
+              // the callback you provide.  They can be:
+              //
+              // 1. Logged into your app ('connected')
+              // 2. Logged into Facebook, but not your app ('not_authorized')
+              // 3. Not logged into Facebook and can't tell if they are logged into
+              //    your app or not.
+              //
+              // These three cases are handled in the callback function.
 
-                  FB.getLoginStatus(function(response) {
-                    statusChangeCallback(response);
-                    if(response.status === 'connected') {
-                      alert('Hello!')
-                      var signedInUserFacebookId = ''
-                      FB.api('/me', function(response) {
-                          signedInUserFacebookId = response.id
-                          $.ajax({
-                            url: window.location.href + 'users/facebookId/' + signedInUserFacebookId,
-                            type: 'GET',
-                            success: function(data) {
-                                console.log(1, data);
-                                if (data != 0) {
-                                    var signedInUser = {user: data}
-                                    login(signedInUser)
-                                }
-                                if (data === 0) {
-                                    signedInUserFullName = response.name
-                                    console.log(2, signedInUserFullName)
-                                    register(signedInUserFacebookId, signedInUserFullName)
-                                }
+              FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+                if(response.status === 'connected') {
+                  var signedInUserFacebookId = ''
+                  FB.api('/me', function(response) {
+                      signedInUserFacebookId = response.id
+                      $.ajax({
+                        url: window.location.href + 'users/facebookId/' + signedInUserFacebookId,
+                        type: 'GET',
+                        success: function(data) {
+                            console.log(1, data);
+                            if (data != 0) {
+                                var signedInUser = {user: data}
+                                login(signedInUser)
                             }
-                          })
-                      });
-                      
-                    }
+                            if (data === 0) {
+                                signedInUserFullName = response.name
+                                console.log(2, signedInUserFullName)
+                                register(signedInUserFacebookId, signedInUserFullName)
+                            }
+                        }
+                      })
                   });
+                  
+                }
+              });
 
-              };
+          };
 
-              // Load the SDK asynchronously
-              (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));
+          // Load the SDK asynchronously
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
 
   
         })
         $('header').toggleClass("hidden");
         var row = '';
-        //row += '<fb:login-button autologoutlink="true"></fb:login-button>'
         row += '<fb:login-button autologoutlink="true" id="fbloginbutton" scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>'
         
         row += '<p>New User?</p>';
-        row += '<button class="js-register">Register</button> <button class="js-demo">Check Out Demo</button>';
+        row += '<button class="js-demo">Check Out Demo</button>';
         $('main').html(row);
         
-        $('.js-register').click(function(event) {
-            register();
-        });
         $('.js-demo').click(function(event) {
             $.ajax({
                 type: 'GET',
